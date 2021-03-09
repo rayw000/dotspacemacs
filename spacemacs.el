7333624515497cadd1911dc31646c6c86f6f03eb
Ray <ray.hackmylife@gmail.com>
2021-03-09 11:56:35 +0800

    theme

--- spacemacs.el
+++ spacemacs.el
@@ -110,6 +110,7 @@ This function should only modify configuration layer settings."
           osx-swap-option-and-command nil)
      syntax-checking
      tern
+     themes-megapack
      (treemacs :variables
                treemacs-use-all-the-icons-theme t)
      unicode-fonts
@@ -277,7 +278,7 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(smyx)
+   dotspacemacs-themes '(seti)
 
    ;; Set the theme for the Spaceline. Supported themes are `spacemacs',
    ;; `all-the-icons', `custom', `doom', `vim-powerline' and `vanilla'. The
