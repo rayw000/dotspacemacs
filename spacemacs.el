1acb0e826c11de6536b0c549eba290396ecbaef5
Ray <ray.hackmylife@gmail.com>
2021-03-19 12:07:55 +0800

    load emacs source code for better help buffer

--- spacemacs.el
+++ spacemacs.el
@@ -548,7 +548,8 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
   (setq
    scroll-bar-mode nil
    ag-highlight-search t
-   default-directory (expand-file-name "~/"))
+   default-directory (expand-file-name "~/")
+   source-directory (expand-file-name "~/repo/open-source/emacs"))
   (when (fboundp 'native-compile)
     (setq comp-deferred-compilation-deny-list '("powerline"))
     (setq package-native-compile t)))
